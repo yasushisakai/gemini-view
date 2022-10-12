4ed24642efc870c5523f58b2b88d070903d65bcd
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-10-12 15:01:09 +0900

    plist hell launchd should be easier

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -s https://mirage.city/uploads/ | rg -o '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}_[0-9]*_[0-9a-z_]*.jpg' | sort > remote.txt
-find ~/code/gemini-view/assets/images -type f | sed 's/^assets\/images\///'  | sort > local.txt
-join -v2 local.txt remote.txt | xargs -I@ curl https://mirage.city/uploads/@ -o ~/code/gemini-view/assets/images/@
-rm local.txt remote.txt
+GEMINIVIEWDIR=/Users/yasushi/code/gemini-view/
+curl -s https://mirage.city/uploads/ | grep -oE '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}_[0-9]*_[0-9a-z_]*.jpg' | sort > $GEMINIVIEWDIR/remote.txt
+find $GEMINIVIEWDIR/assets/images -type f | sed 's/^assets\/images\///'  | sort > $GEMINIVIEWDIR/local.txt
+join -v2 $GEMINIVIEWDIR/local.txt $GEMINIVIEWDIR/remote.txt | xargs -I@ curl https://mirage.city/uploads/@ -o $GEMINIVIEWDIR/assets/images/@
+rm $GEMINIVIEWDIR/local.txt $GEMINIVIEWDIR/remote.txt
