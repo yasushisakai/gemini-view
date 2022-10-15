f51ba1f99369417e7b552e60a93d0ed10ae02fe8
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-10-15 16:40:18 +0900

    limits to 100 images, uses ENVVAR

--- download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-GEMINIVIEWDIR=/Users/yasushi/code/gemini-view/
-curl -s https://mirage.city/uploads/ | grep -oE '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}_[0-9]*_[0-9a-z_]*.jpg' | sort > $GEMINIVIEWDIR/remote.txt
-find $GEMINIVIEWDIR/assets/images -type f | sed 's/^assets\/images\///'  | sort > $GEMINIVIEWDIR/local.txt
-join -v2 $GEMINIVIEWDIR/local.txt $GEMINIVIEWDIR/remote.txt | xargs -I@ curl https://mirage.city/uploads/@ -o $GEMINIVIEWDIR/assets/images/@
-rm $GEMINIVIEWDIR/local.txt $GEMINIVIEWDIR/remote.txt
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 use nannou::io::walk_dir;
 use nannou::prelude::*;
+use std::env;
 use std::path::PathBuf;
 use std::time::Duration;
 
-const FILENAME: &str = "scene_3.jpg";
-
 struct Model {
     interval: Duration,
+    filename: String,
     assets: PathBuf,
     index: usize,
-    images: Vec<String>,
-    texture: wgpu::Texture,
+    images: Vec<PathBuf>,
+    texture: Option<wgpu::Texture>,
 }
 
 impl Model {
     pub fn inc_image(&mut self, app: &App) {
         self.index += 1;
-        if self.index >= self.images.len() {
+
+        if self.images.len() == 0 || self.index >= self.images.len() {
+            self.images = image_files(&app, &self.assets, &self.filename);
             self.index = 0;
-            let mut images: Vec<String> = walk_dir(self.assets.join("images"))
-                .into_iter()
-                .map(|dir| {
-                    let path = dir.unwrap().into_path();
-                    path.to_string_lossy().to_string()
-                })
-                .filter(|p| p.ends_with(FILENAME))
-                .collect();
-            images.sort();
-            let past = self.images.len();
-            if past != images.len() {
-                self.images = images;
-                println!("len is now: {}", self.images.len());
+             
+            if let Some(img) = self.images.last() {
+                println!("last image: {}", img.display());
+            }
+
+        }
+
+        if !self.images.is_empty() {
+            let img = &self.images[self.index];
+
+            if let Ok(texture) = wgpu::Texture::from_path(app, img) {
+                self.texture = Some(texture);
             }
         }
-        let img = &self.images[self.index];
-        self.texture = wgpu::Texture::from_path(app, img).unwrap();
     }
 }
 
@@ -44,51 +43,59 @@ fn main() {
 
 fn update(app: &App, model: &mut Model, update: Update) {
     model.interval += update.since_last;
-    if model.interval.as_millis() > 80 {
+    if model.interval.as_millis() > 100 {
         model.interval = Duration::from_secs(0);
         model.inc_image(app);
     }
 }
 
-fn model(app: &App) -> Model {
-    app.new_window().fullscreen().view(view).build().unwrap();
-    app.set_loop_mode(LoopMode::rate_fps(60.0));
+fn image_files(app: &App, assets_path: &PathBuf, filename: &str) -> Vec<PathBuf> {
+    let assets = app.assets_path().expect("can't find assets folder");
 
-    let assets = app.assets_path().unwrap();
-
-    let mut images: Vec<String> = walk_dir(assets.join("images"))
+    let mut files: Vec<PathBuf> = walk_dir(assets.join("images"))
         .into_iter()
-        .map(|dir| {
-            let path = dir.unwrap().into_path();
-            format!("{}", path.display())
-        })
-        .filter(|p| p.ends_with(FILENAME))
+        .map(|dir| dir.unwrap().into_path())
+        .filter(|p| p.to_string_lossy().ends_with(filename))
         .collect();
 
-    images.sort();
+    files.sort();
 
-    let texture = wgpu::Texture::from_path(app, &images[1]).unwrap();
-    let mut model = Model {
+    if files.len() > 100 {
+        files[(files.len()-100)..files.len()].to_vec()
+    } else {
+        files
+    }
+}
+
+fn model(app: &App) -> Model {
+    app.new_window().fullscreen().view(view).build().unwrap();
+    app.main_window().set_cursor_visible(false);
+    app.set_loop_mode(LoopMode::rate_fps(60.0));
+
+    let filename = match env::var("GEMINIFILENAME") {
+        Ok(filename) => filename,
+        Err(_) => "default.jpg".to_string(),
+    };
+
+    let assets = app.assets_path().expect("can't find assets");
+
+    Model {
+        filename,
         interval: Duration::from_secs(0),
         index: 0,
         assets,
-        texture,
-        images,
-    };
-    model.inc_image(app);
-    model
+        texture: None,
+        images: Vec::new(),
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     frame.clear(BLACK);
     let draw = app.draw();
-    draw.texture(&model.texture).wh(vec2(1980f32, 1080f32));
 
-    let rate = (model.index as f32) / (model.images.len() as f32);
-    let win = app.window_rect();
-    let width = win.w() * rate;
-    let bar = Rect::from_w_h(width, 3f32).top_left_of(win);
+    if let Some(tex) = &model.texture {
+        draw.texture(tex).wh(vec2(1980f32, 1080f32));
+    }
 
-    draw.rect().xy(bar.xy()).wh(bar.wh()).color(YELLOW);
     draw.to_frame(app, &frame).unwrap();
 }
